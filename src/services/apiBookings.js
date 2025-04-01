da264e042b33b408a030c13cc3436541918cc635
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-04-02 01:15:23 +0200

    Bookings Table

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,6 +1,15 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
+export async function getBookings() {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select(
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
+    );
+  return data;
+}
+
 export async function getBooking(id) {
   const { data, error } = await supabase
     .from("bookings")
